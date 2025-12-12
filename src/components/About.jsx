095f661a072bf4428e597e5efae999d902513066
Samiksha Satthy <satthy.samiksha@gmail.com>
2025-12-12 08:55:15 -0500

    updated contact info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import headshot from "../assets/img/headshot.JPG";
+import Contact from './Contact'
 
 function About() {
   return (
@@ -8,10 +9,10 @@ function About() {
       id="about"
     >
       <div className="flex flex-col gap-10">
-        <h1 className="text-transparent bg-clip-text bg-linear-to-r from-purple-800 to-purple-300 p-4">
+        <h1 className="text-transparent bg-clip-text bg-linear-to-r from-purple-800 to-purple-300 p-4 text-center">
           <b>About Me</b>
         </h1>
-        <p className="text-white lg:w-125 self-center mx-4">
+        <p className="text-white lg:w-125 text-center mx-4">
           Hello! 👋🏽 My name is Samiksha and I am a Computer Engineering Student
           at the University of Waterloo. I am dedicated to making a positive
           impact through the use of cutting-edge technology. Additionally, I am
@@ -21,6 +22,7 @@ function About() {
           fellow enthusiasts, professionals, and anyone who shares my passions.
           📬
         </p>
+        <Contact/>
       </div>
       <img
         className="lg:w-150 h-max w-100 mx-4 group bg-card rounded-xl overflow-hidden shadow-md bg-[#1d1836]/90 backdrop-blur-md transition-all duration-300 transform hover:-translate-y-1 hover:shadow-lg hover:border-[#8c52ff] border border-white/10 "
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HiArrowSmDown } from "react-icons/hi";
 import { Link } from "react-scroll";
 import Contact from "./Contact";
+import {FaEnvelope} from "react-icons/fa"
 
 function Title() {
   return (
@@ -14,8 +15,14 @@ function Title() {
         <h1 className="text-transparent bg-clip-text bg-linear-to-r from-purple-800 to-purple-300 py-4 lg:p-4">
           <b>Samiksha Satthy</b>
         </h1>
-        <h2 className="lg:font-bold">Computer Engineering @ UWaterloo</h2>
+        <h2 className="lg:font-bold mb-7">Computer Engineering @ UWaterloo</h2>
+        <div className="flex flex-row items-center gap-5 border-3 rounded-2xl px-8 border-purple-300 text-transparent bg-linear-to-r from-[#e59cff] via-[#ba9cff] to-[#9cb2ff] bg-clip-text backdrop-blur-md shadow-[inset_0_-7px_11px_#a48fff1f] transition duration-300 ease-in-out hover:shadow-[0_0_20px_#d498fb] hover:border-[#d498fb]">
+          <FaEnvelope size={24} className="text-center text-white"/>
+          <h3>ssatthy@uwaterloo.ca</h3>
+        </div>
       </div>
+
+
 
       <Link
         to="about"
