29510e81c8bfb4499cc4078d229e53bd29173fb6
Samiksha Satthy <satthy.samiksha@gmail.com>
2025-12-12 09:05:12 -0500

    linked email

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { HiArrowSmDown } from "react-icons/hi";
 import { Link } from "react-scroll";
 import Contact from "./Contact";
-import {FaEnvelope} from "react-icons/fa"
+import { FaEnvelope } from "react-icons/fa";
 
 function Title() {
   return (
@@ -16,13 +16,13 @@ function Title() {
           <b>Samiksha Satthy</b>
         </h1>
         <h2 className="lg:font-bold mb-7">Computer Engineering @ UWaterloo</h2>
-        <div className="flex flex-row items-center gap-5 border-3 rounded-2xl px-8 border-purple-300 text-transparent bg-linear-to-r from-[#e59cff] via-[#ba9cff] to-[#9cb2ff] bg-clip-text backdrop-blur-md shadow-[inset_0_-7px_11px_#a48fff1f] transition duration-300 ease-in-out hover:shadow-[0_0_20px_#d498fb] hover:border-[#d498fb]">
-          <FaEnvelope size={24} className="text-center text-white"/>
-          <h3>ssatthy@uwaterloo.ca</h3>
-        </div>
+        <a href="mailto:ssatthy@uwaterloo.ca" target="_blank">
+          <div className="flex flex-row items-center gap-5 border-3 rounded-2xl px-8 border-purple-300 text-transparent bg-linear-to-r from-[#e59cff] via-[#ba9cff] to-[#9cb2ff] bg-clip-text backdrop-blur-md shadow-[inset_0_-7px_11px_#a48fff1f] transition duration-300 ease-in-out hover:shadow-[0_0_20px_#d498fb] hover:border-[#d498fb]">
+            <FaEnvelope size={24} className="text-center text-white" />
+            <h3>ssatthy@uwaterloo.ca</h3>
+          </div>
+        </a>
       </div>
-
-
 
       <Link
         to="about"
